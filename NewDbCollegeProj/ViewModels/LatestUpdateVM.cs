bde9750835d28317cbd7ba2687e878064ce47596
EI13108 <EI13108@ELW5219>
2023-06-09 16:46:27 +0530

    Add project files.

---
+++ NewDbCollegeProj/ViewModels/LatestUpdateVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewDbCollegeProj.ViewModels
+{
+    public class LatestUpdateVM
+    {
+        
+        public string Information { get; set; }
+        public string Name { get; set; }
+        public DateTime Created_Date { get; set; }
+        
+    }
+}
